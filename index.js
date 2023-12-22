7cd6b095a53d93e654200d9e5344bdff59d5108b
billal-webdev <billal-webdev@gmail.com>
2023-12-22 09:09:23 +0600

    added api to post and get task

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 9000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0ak1okw.mongodb.net/?retryWrites=true&w=majority`;
 
-
 //Middlewares
 app.use(cors());
 app.use(cookieParser());
+app.use(express.json());
 
-
-app.get("/", (req, res)=>{
-    res.send(`Task management server is running on port ${port}`)
-})
-
-
-
-
+app.get("/", (req, res) => {
+  res.send(`Task management server is running on port ${port}`);
+});
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,16 +23,44 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    //DB Operations
+    const tasksCollection = client.db("taskRDB").collection("tasks");
+
+    app.post("/tasks", async (req, res) => {
+      const newTask = req.body;
+      try {
+        const result = await tasksCollection.insertOne(newTask);
+        res
+          .status(200)
+          .json({ message: "Successfully added a new task", code: 200 });
+      } catch (error) {
+        res
+          .status(500)
+          .json({ error: true, message: "There was server side error" });
+      }
+    });
+
+    app.get("/tasks", async(req, res)=>{
+      const {email} = req.query;
+
+      const tasks = await tasksCollection.find({email}).toArray();
+      res.send(tasks)
+    })
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -45,10 +68,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-app.listen(port, ()=>{
-    console.log(`Task management server is listening at port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Task management server is listening at port ${port}`);
+});
